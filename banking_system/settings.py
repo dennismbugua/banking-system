01660f79d18c3572c7c5823e486f366a8c46ee38
dennismbugua <dennis@starlaunch.org>
2024-04-12 19:56:54 +0300

    STATIC_ROOT = os.path.join(BASE_DIR, 'static')

--- banking_system/settings.py
+++ banking_system/settings.py
@@ -131,10 +131,10 @@ USE_TZ = True
 
 
 STATIC_URL = "static/"
-STATICFILES_DIRS = [
-    BASE_DIR / "static/",
-]
-
+# STATICFILES_DIRS = [
+#     BASE_DIR / "static/",
+# ]
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 ACCOUNT_NUMBER_START_FROM = 1000000000
