11e85b713bc4eb8b8bfcac61889157a1d0ab3e07
dennismbugua <dennis@starlaunch.org>
2024-04-13 11:58:32 +0300

    "buildCommand": "./build_files.sh"

--- build_files.sh
+++ build_files.sh
@@ -1,3 +1,7 @@
-# build_files.sh
+#!/bin/bash
+
+# Install Python dependencies
 pip install -r requirements.txt
-python3.9 manage.py collectstatic
+
+# Run collectstatic without interactive prompting
+python3.9 manage.py collectstatic --noinput
